7f55334ab3c0d850a60240b68ae39db9c82b1537
Steven Giampapa <stevengiampapa@arizona.edu>
2023-03-31 15:14:41 -0700

    added dice roll

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import random
 from discord.ext import commands
 
 class Utility(commands.Cog):
@@ -8,6 +9,12 @@ class Utility(commands.Cog):
     @commands.command()
     async def eric(self, ctx):
         await ctx.send("Fuck you Eric.")
+
+    @commands.command()
+    async def dice_roll(self, ctx):
+        random_number=random.randint(1,12)
+        await ctx.send(str(random_number))
+        
 
 async def setup(bot):
     await bot.add_cog(Utility(bot))
