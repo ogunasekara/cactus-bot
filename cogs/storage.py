473b1565a16f29751facab2324f70e76c64aea03
Oshadha Gunasekara <oshadha123@gmail.com>
2023-04-07 15:51:33 -0500

    fix economy functionality

--- cogs/storage.py
+++ cogs/storage.py
@@ -1,28 +1,31 @@
 import pickle
+import time
 
 from dotenv import load_dotenv
 from discord.ext import commands
+from data.datastore import DataStore
 
 PICKLE_FILEPATH = './data.pickle'
-
-class DataStore:
-    def __init__(self):
-        self.calendar_cache = {}
-        self.birthday_cache = {}
-        self.log_cache = {}
         
 class Storage(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.data: DataStore = self.load_data()
-        self.update_pickle()
 
     def load_data(self):
         try:
-            return pickle.load(open(PICKLE_FILEPATH, "rb"))
-        except:
+            file = open(PICKLE_FILEPATH, "rb")
+            data = pickle.load(file)
+            file.close()
+            return data
+        except Exception as e:
+            print(e)
             print("No existing pickle. Creating new pickle file.")
-            return DataStore()
+            data = DataStore()
+            file = open(PICKLE_FILEPATH, "wb")
+            pickle.dump(data, file)
+            file.close()
+            return data
     
     def get_calendar_cache(self):
         return self.data.calendar_cache
@@ -37,16 +40,22 @@ class Storage(commands.Cog):
     def set_birthday_cache(self, birthday_cache):
         self.data.birthday_cache = birthday_cache
         self.update_pickle()
-    
-    def update_pickle(self):
-        pickle.dump(self.data, open(PICKLE_FILEPATH, "wb"))
+
+    def get_voice_log_cache(self):
+        return self.data.log_cache
 
     def set_voice_log_cache(self, logs):
         self.data.log_cache = logs
         self.update_pickle()
-
-    def get_voice_log_cache(self):
-        return self.data.log_cache
+    
+    def update_pickle(self):
+        try:
+            file = open(PICKLE_FILEPATH, "wb")
+            pickle.dump(self.data, file)
+            file.close()
+            print('updated pickle file')
+        except Exception as e:
+            print(e)
 
 async def setup(bot):
     await bot.add_cog(Storage(bot))
